81dadd22247016ca7aa081db71da5b5ed9c13a6c
三咲智子 <sxzz@sxzz.moe>
2022-12-22 13:46:01 +0800

    test: add

--- packages/compiler-sfc/__tests__/compileScript.spec.ts
+++ packages/compiler-sfc/__tests__/compileScript.spec.ts
@@ -904,6 +904,7 @@ const emit = defineEmits(['a', 'b'])
         literalUnionNumber: 1 | 2 | 3 | 4 | 5
         literalUnionMixed: 'foo' | 1 | boolean
         intersection: Test & {}
+        intersection2: 'foo' & ('foo' | 'bar')
         foo: ((item: any) => boolean) | null
       }>()
       </script>`)
@@ -939,6 +940,7 @@ const emit = defineEmits(['a', 'b'])
         `literalUnionMixed: { type: [String, Number, Boolean], required: true }`
       )
       expect(content).toMatch(`intersection: { type: Object, required: true }`)
+      expect(content).toMatch(`intersection2: { type: String, required: true }`)
       expect(content).toMatch(`foo: { type: [Function, null], required: true }`)
       expect(bindings).toStrictEqual({
         string: BindingTypes.PROPS,
@@ -966,6 +968,7 @@ const emit = defineEmits(['a', 'b'])
         literalUnionNumber: BindingTypes.PROPS,
         literalUnionMixed: BindingTypes.PROPS,
         intersection: BindingTypes.PROPS,
+        intersection2: BindingTypes.PROPS,
         foo: BindingTypes.PROPS
       })
     })
